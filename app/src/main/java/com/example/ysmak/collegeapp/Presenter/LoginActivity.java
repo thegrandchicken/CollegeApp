bd5dcdad4acead9fe224ecd10bdde5b9f7a34b23
ysmak <thegrandchicken@gmail.com>
2017-01-23 13:14:09 -0800

    Commit 23-1-17

--- app/src/main/java/com/example/ysmak/collegeapp/Presenter/LoginActivity.java
+++ app/src/main/java/com/example/ysmak/collegeapp/Presenter/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.csaper6.collegeapp.Presenter;
+package com.example.ysmak.collegeapp.Presenter;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,20 +9,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.backendless.UserService;
 import com.backendless.Backendless;
 import com.backendless.BackendlessUser;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
-import com.example.csaper6.collegeapp.R;
+import com.example.ysmak.collegeapp.R;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        Intent intent = getIntent();
 
         final EditText username = (EditText) findViewById(R.id.editText_username);
         final EditText password = (EditText) findViewById(R.id.editText_password);
@@ -33,41 +30,36 @@ public class LoginActivity extends AppCompatActivity {
         Backendless.initApp(this, "2EE6BCB6-1C79-C7EA-FF53-D88FFEAA3A00",
                 secretKey, "v1"); // where to get the argument values for this call
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchMainActivity();
-            }
-        });
         cna.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CreateAccountActivity.class);
                 startActivity(intent);
-
-                /*public void Backendless.UserService.login( String username,
-                        String password, loginUserCallback());
-
-                public void Backendless.UserService.login( String username
-                        String password, loginUserCallback(), boolean stayLoggedIn);*/
-
-                Backendless.UserService.login(username.getText().toString(), password.getText().toString());
             }
         });
-    }
 
-    private AsyncCallback<BackendlessUser> loginUserCallback() {
-        return new AsyncCallback<BackendlessUser>() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void handleResponse(BackendlessUser user) {
-                // user has been logged in
-            }
+            public void onClick(View view) {
 
-            @Override
-            public void handleFault(BackendlessFault fault) {
-                // login failed, to get the error code call fault.getCode()
+                Backendless.UserService.login( username.getText().toString(),
+                        password.getText().toString(), new AsyncCallback<BackendlessUser>()
+                        {
+                            public void handleResponse( BackendlessUser user )
+                            {
+                                // user has been logged in
+                                launchMainActivity();
+                            }
+
+                            public void handleFault( BackendlessFault fault )
+                            {
+                                // login failed, to get the error code call fault.getCode()
+                                Toast.makeText(LoginActivity.this, "Error: " + fault.getCode(), Toast.LENGTH_SHORT).show();
+                                //TODO: what is an internal client exception????
+                            }
+                        });
             }
-        };
+        });
     }
 
     private void launchMainActivity() {
